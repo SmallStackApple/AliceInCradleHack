751ee3a291568d19104bada14efbf63a4cdfad7c
SmallStackApple <178256479+SmallStackApple@users.noreply.github.com>
2026-04-12 12:10:05 +0800

    增加类kotlin语法支持(?)

---
+++ AliceInCradleHack/Utils/KotlinExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace AliceInCradleHack.Utils
+{
+    //何意味？
+    public static class KotlinExtension
+    {
+        // ==============================
+        // Kotlin: inline fun T.apply(block: T.() -> Unit): T
+        // ==============================
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T apply<T>(this T self, Action<T> block)
+        {
+            block(self);
+            return self;
+        }
+
+        // ==============================
+        // Kotlin: inline fun T.also(block: (T) -> Unit): T
+        // ==============================
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T also<T>(this T self, Action<T> block)
+        {
+            block(self);
+            return self;
+        }
+
+        // ==============================
+        // Kotlin: inline fun <T, R> T.let(block: (T) -> R): R
+        // ==============================
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static R let<T, R>(this T self, Func<T, R> block) => block(self);
+
+        // ==============================
+        // Kotlin: inline fun <T, R> T.run(block: T.() -> R): R
+        // ==============================
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static R run<T, R>(this T self, Func<T, R> block) => block(self);
+    }
+}
